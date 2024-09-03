140c17c76ee38ad137a0b38b57b1a02ee2206ba6
Varad <166056504+VaradPT@users.noreply.github.com>
2024-09-03 19:08:04 +0530

    Add files via upload

---
+++ BsicSelSort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i,n,j,key;
+	FILE *file = fopen("sel.txt","r"); //Enter the file name 
+	fscanf(file,"%d",&n);
+
+	int *Arr=(int*)malloc(n*sizeof(int));
+
+	for(i=0;i<n;i++)
+	{
+		fscanf(file,"%d",&Arr[i]);
+	}
+	fclose(file);
+
+	for(i=0;i<n-1;i++)
+	{
+		key=i;
+		for(j=i+1;j<n;j++)
+		{
+			if(Arr[j]<Arr[key])
+			{
+			key=j;
+			}
+		}
+		int temp=Arr[key];
+		Arr[key]=Arr[i];
+		Arr[i]=temp;
+	}
+	printf("Sorted Array is:\n");
+	for(i=0;i<n;i++)
+	{
+	printf("%d \t",Arr[i]);
+	}
+	printf("\n");
+
+return 0;
+}
